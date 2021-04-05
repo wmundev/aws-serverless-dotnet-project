f84b13d33c915b497ffc5f688f5f7a347f985db0
Wilson Mun <30316250+wmundev@users.noreply.github.com>
2021-04-05 22:12:58 +1000

    add prime number function

--- src/HelloWorld/Function.cs
+++ src/HelloWorld/Function.cs
@@ -1,22 +1,19 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Net.Http;
-using Newtonsoft.Json;
-
-using Amazon.Lambda.Core;
+using System.Threading.Tasks;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using JsonSerializer = Amazon.Lambda.Serialization.Json.JsonSerializer;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+[assembly: LambdaSerializer(typeof(JsonSerializer))]
 
 namespace HelloWorld
 {
-
     public class Function
     {
-
         private static readonly HttpClient client = new HttpClient();
 
         private static async Task<string> GetCallingIP()
@@ -24,9 +21,10 @@ namespace HelloWorld
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Add("User-Agent", "AWS Lambda .Net Client");
 
-            var msg = await client.GetStringAsync("http://checkip.amazonaws.com/").ConfigureAwait(continueOnCapturedContext:false);
+            var msg = await client.GetStringAsync("http://checkip.amazonaws.com/")
+                .ConfigureAwait(continueOnCapturedContext: false);
 
-            return msg.Replace("\n","");
+            return msg.Replace("\n", "");
         }
 
         private bool CalculatePrimeNumber(int num)
@@ -45,29 +43,31 @@ namespace HelloWorld
             return true;
         }
 
-        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent,
+            ILambdaContext context)
         {
             string region = Environment.GetEnvironmentVariable("SqsQueueArn");
             context.Logger.LogLine("region" + region);
-            
+
             int requestBody = int.Parse(apigProxyEvent.Body);
 
             bool isPrime = CalculatePrimeNumber(requestBody);
 
             context.Logger.LogLine($"Number {requestBody} prime is {isPrime.ToString()}");
-            
+
             var location = await GetCallingIP();
             var body = new Dictionary<string, string>
             {
-                { "message", "hello world" },
-                { "location", location }
+                {"message", "hello world"},
+                {"location", location},
+                {"isPrimeNumber", isPrime.ToString()}
             };
 
             return new APIGatewayProxyResponse
             {
                 Body = JsonConvert.SerializeObject(body),
                 StatusCode = 200,
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+                Headers = new Dictionary<string, string> {{"Content-Type", "application/json"}}
             };
         }
     }
